a48f8c1981ee292a43569dbe97e114565de8b2b0
Shruti Kumari <shruti.kumari@ingenxtec.com>
2024-04-10 07:40:36 +0000

    first commit

--- webapp/controller/BusinessPartner.controller.js
+++ webapp/controller/BusinessPartner.controller.js
@@ -1,173 +1,37 @@
 
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageBox"
 ],
-function (Controller, MessageBox) {
+  function (Controller, MessageBox) {
     "use strict";
-     let selectedDataArr = [];
+    let selectedDataArr = [];
     return Controller.extend("com.ingenx.nauti.nauticalvendor.controller.BusinessPartner", {
-        onInit: function () {
-            
-            var oView = this.getView();
-            var oSmartTable = oView.byId("LineItemsSmartTable");
-            var oInnerTable = oSmartTable.getTable();
-            oInnerTable.attachRowSelectionChange(this.onRowSelectionChange, this);
-        },
- 
-        onRowSelectionChange: function(oEvent) {
-            // Get the selected row(s)
-            var aSelectedIndices = oEvent.getParameter("rowIndices");
-            var oTable = oEvent.getSource();
-            let payLoad = {}
-           
-            // Get the selected items data
-            var aSelectedData = aSelectedIndices.map(function(index) {
-                return oTable.getContextByIndex(index).getObject();
-            });
- 
-            // Display data of selected row(s) in console
-            console.log("Selected row(s) data:",  aSelectedData[0]);
-            selectedDataArr.push(aSelectedData[0]);
-            console.log(selectedDataArr);
-            
-        },
-
-     onSubmit : function(oEvent){
-        //   let payLoad  = {
-        //        "Lifnr" : "",
-        // "PartnerRole" : "",
-        // "Anred" : "",
-        // "Name1" : "",
-        // "Name2" : "",
-        // "Name3" : "",
-        // "Sort1" : "",
-        // "StrSuppl1" : "",
-        // "StrSuppl2" : "",
-        // "HouseNum1" : "",
-        // "Stras" : "",
-        // "Pstlz" : "",
-        // "Ort01" : "",
-        // "Land1" : "",
-        // "Regio" : "",
-        // "TimeZone" : "",
-        // "Spras" : "",
-        // "Telf1" : "",
-        // "Telf2" : "",
-        // "Telfx" : "",
-        // "SmtpAddr" : "",
-        // "Erdat" : null,
-        // "DateTo" : null
-      
-        //   }
-          let oModel = this.getOwnerComponent().getModel("MASTER_BTP_SRV");
-          console.log("My model",oModel);
-
-          let payLoad = { 
-            
-            "Lifnr" : "2100000009",
-            "PartnerRole" : "",
-            "Anred" : "Company",
-            "Name1" : "The Shipping Corporation Of India",
-            "Name2" : "Limited",
-            "Name3" : "",
-            "Sort1" : "",
-            "StrSuppl1" : "",
-            "StrSuppl2" : "",
-            "HouseNum1" : "",
-            "Stras" : "1st floor, Chandralok Buillding,",
-            "Pstlz" : "110001",
-            "Ort01" : "Delhi",
-            "Land1" : "IN",
-            "Regio" : "30",
-            "TimeZone" : "",
-            "Spras" : "EN",
-            "Telf1" : "0124-3345678",
-            "Telf2" : "9911267017",
-            "Telfx" : "0124-3345678",
-            "SmtpAddr" : "",
-            "Erdat" : "\/Date(1661126400000)\/",
-            "DateTo" : null
-          }
-
-          for( let i=0; i< selectedDataArr.length ; i++){
-            payLoad = {
-              "Lifnr" : selectedDataArr[i].Supplier,
-              "PartnerRole" : "",
-              "Anred" : "",
-              "Name1" : selectedDataArr[i].SupplierName,
-              "Name2" : "",
-              "Name3" : "",
-              "Sort1" : "",
-              "StrSuppl1" : "",
-              "StrSuppl2" : "",
-              "HouseNum1" : "",
-              "Stras" : selectedDataArr[i].StreetName,
-              "Pstlz" : selectedDataArr[i].PostalCode,
-              "Ort01" : selectedDataArr[i].CityName,
-              "Land1" : selectedDataArr[i].Country,
-              "Regio" : null,
-              "TimeZone" : null,
-              "Spras" : null,
-              "Telf1" : selectedDataArr[i].PhoneNumber1,
-              "Telf2" : selectedDataArr[i].PhoneNumber2,
-              "Telfx" : null,
-              "SmtpAddr" : null,
-              "Erdat" : null,
-              "DateTo" : null
-            }
-
-          //   let payLoad = {  "Lifnr" : "2100000009",
-          //   "PartnerRole" : "",
-          //   "Anred" : "Company",
-          //   "Name1" : "The Shipping Corporation Of India",
-          //   "Name2" : "Limited",
-          //   "Name3" : "",
-          //   "Sort1" : "",
-          //   "StrSuppl1" : "",
-          //   "StrSuppl2" : "",
-          //   "HouseNum1" : "",
-          //   "Stras" : "1st floor, Chandralok Buillding,",
-          //   "Pstlz" : "110001",
-          //   "Ort01" : "Delhi",
-          //   "Land1" : "IN",
-          //   "Regio" : "30",
-          //   "TimeZone" : "",
-          //   "Spras" : "EN",
-          //   "Telf1" : "0124-3345678",
-          //   "Telf2" : "9911267017",
-          //   "Telfx" : "0124-3345678",
-          //   "SmtpAddr" : "",
-          //   "Erdat" : "\/Date(1661126400000)\/",
-          //   "DateTo" : null
-          // }
-          //   console.log(payLoad);
-          //   oModel.create( "/BusinessPartnerSet", payLoad, {
-          //     success : (data, response) =>{
-          //       MessageBox.success("Data successfully created",response);
-          //       oModel.refresh();
-          //     },
-          //     error :  function( error){
-          //       MessageBox.error("Error:",error)
-          //     }
-          //   })
-
-              
-          // }
-          console.log(payLoad);
-
-          oModel.create( "/BusinessPartnerSet", payLoad, {
-            success : (data, response) =>{
-              MessageBox.success("Data successfully created",response);
-              oModel.refresh();
-            },
-            error :  function( error){
-              MessageBox.error("Error:",error)
-            }
-          })
-        }
+      onInit: function () {
+        // var oView = this.getView();
+        // var oTable = oView.byId("LineItemsSmartTable");
+        // oTable.attachRowSelectionChange(this.onRowSelectionChange, this);
       },
-   
+
+      onRowSelectionChange: function (oEvent) {
+        // You can still handle row selection changes here if needed
+      },
+
+      onSubmit: function () {
+        var oSmartTable = this.byId("LineItemsSmartTable");
+        console.log(oSmartTable);
+        var aSelectedContexts = oSmartTable.getSelectedContexts();
+
+        if (aSelectedContexts && aSelectedContexts.length > 0) {
+          for (var i = 0; i < aSelectedContexts.length; i++) {
+            var oSelectedRowData = aSelectedContexts[i].getObject();
+            console.log("Selected Row Data:", oSelectedRowData);
+          }
+        } else {
+          console.log("No rows selected.");
+        }
+      }
+
+
     });
-});
- 
+  });
